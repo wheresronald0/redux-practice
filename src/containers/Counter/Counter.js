b1089caee825e157e0fcadcd513961df5a95f42b
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-11-27 14:40:32 -0500

    added a store results button

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -22,6 +22,17 @@ class Counter extends Component {
           label="Subtract 5"
           clicked={this.props.onSubtractCounter}
         />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map(result => {
+            return (
+              <li key={result.id} onClick={this.props.onDeleteResult}>
+                {result.value}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
@@ -29,16 +40,19 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter //from reducer file
+    ctr: state.counter, //from reducer file
+    storedResults: state.results
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }), //connect method is calling the mapDispatch function, returning/making available each of these that is "type' in the reducer, which I can passed additional payload for the reducer to have/control, and then return back after it does what it does
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", value: 5 }),
-    onSubtractCounter: () => dispatch({ type: "SUBTRACT", value: 5 })
+    onSubtractCounter: () => dispatch({ type: "SUBTRACT", value: 5 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResult: () => dispatch({ type: "DELETE_RESULT" })
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,29 +1,46 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     return {
+      ...state, //use the spread opperator to make copy of original state, while only updting the counter, and not results, i.e. what you designate to be overridden
+      //could also do: const newState = Object.assign({}, state) to make a copy, but spread is better
       counter: state.counter + 1
     };
-  }
-  if (action.type === "DECREMENT") {
+  } else if (action.type === "DECREMENT") {
     return {
+      ...state,
       counter: state.counter - 1
     };
-  }
-  if (action.type === "ADD") {
+  } else if (action.type === "ADD") {
     return {
+      ...state,
       counter: state.counter + action.value
     };
-  }
-  if (action.type === "SUBTRACT") {
+  } else if (action.type === "SUBTRACT") {
     return {
+      ...state,
       counter: state.counter - action.value
     };
+  } else if (action.type === "STORE_RESULT") {
+    return {
+      ...state,
+      results: state.results.concat({ id: new Date(), value: state.counter }) //concat instead of push as an immutable way (push will update the original state- no bueno!)
+    }; //(state.counter) worked but I needed a key for the <li> in Counter.js
   }
   return state;
 };
 
 export default reducer;
+
+//can also use a switch statement with cases, instead of mulitple if statements:
+//switch (action.type) {
+//   case "INCREMENT":
+//   return {
+//     counter:statecounter + 1
+//   }
+// }
+// and so on...
